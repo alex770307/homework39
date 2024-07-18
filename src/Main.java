c5af5e0afc826e95a3f7f0e94ca7efc19a29159e
alex_ <alex_7377@hotmail.com>
2024-07-19 01:14:41 +0200

    third commit

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,22 @@ public class Main {
     public static void main(String[] args) {
 
         List<Programminglanguage> languages = new ArrayList<>();
-        languages.add(new Programminglanguage("Java", "James Gosling", LocalDate.of(1995, 5, 23)));
-        languages.add(new Programminglanguage("C", "Dennis Ritchie", LocalDate.of(1972, 5, 1)));
-        languages.add(new Programminglanguage("Python", "Guido van Rossum", LocalDate.of(1991, 2, 20)));
-        languages.add(new Programminglanguage("C++", "Bjarne Stroustrup", LocalDate.of(1985, 10, 1)));
-        languages.add(new Programminglanguage("JavaScript", "Brendan Eich", LocalDate.of(1995, 12, 4)));
-        languages.add(new Programminglanguage("Fortran", "John Backus", LocalDate.of(1957, 1, 1)));
+        languages.add(new Programminglanguage
+                ("Java", "James Gosling", LocalDate.of(1995, 5, 23)));
+        languages.add(new Programminglanguage
+                ("C", "Dennis Ritchie", LocalDate.of(1972, 5, 1)));
+        languages.add(new Programminglanguage
+                ("Python", "Guido van Rossum", LocalDate.of(1991, 2, 20)));
+        languages.add(new Programminglanguage
+                ("C++", "Bjarne Stroustrup", LocalDate.of(1985, 10, 1)));
+        languages.add(new Programminglanguage
+                ("JavaScript", "Brendan Eich", LocalDate.of(1995, 12, 4)));
+        languages.add(new Programminglanguage
+                ("Fortran", "John Backus", LocalDate.of(1957, 1, 1)));
 
         System.out.println("Все языки из списка:");
         languages.forEach(System.out::println);
+
         Programminglanguage oldestLanguage = languages.stream()
                 .min(Comparator.comparing(Programminglanguage::getDateOfFirstVersion))
                 .orElse(null);
